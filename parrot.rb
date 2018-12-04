7808dc4ab7ee220725071674ce01b89ebcb73573
Kyle  Beckstead <kyle21beckstead@gmail.com>
2018-12-04 21:12:19 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,13 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot(argument ="Squawk!")
+puts "Squawk!"
+
+end
+
+def favorite_color(color="blue")
+	puts "My favorite color is #{color}"
+end
+
+
+favorite_color("red") -> this would output "My favorite color is red"
+
+favorite_color() -> this would output "My favorite color is blue"
